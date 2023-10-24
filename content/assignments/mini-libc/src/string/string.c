9481eb59cbfa322316060836a4ddf722a3151f6f
SpottedIonut <120397620+SpottedIonut@users.noreply.github.com>
2023-10-24 19:48:32 +0300

    test

--- content/assignments/mini-libc/src/string/string.c
+++ content/assignments/mini-libc/src/string/string.c
@@ -5,37 +5,72 @@
 char *strcpy(char *destination, const char *source)
 {
 	/* TODO: Implement strcpy(). */
+	size_t length = strlen(source);
+	// copiem pana la (length + 1) pentru a copia si terminatorul
+	for (size_t i = 0; i < length + 1; i++)
+		destination[i] = source[i];
 	return destination;
 }
 
 char *strncpy(char *destination, const char *source, size_t len)
 {
 	/* TODO: Implement strncpy(). */
+	for (size_t i = 0; i < len; i++) {
+		destination[i] = source[i];
+		if (destination[i] == NULL)
+			return destination;
+	}
 	return destination;
 }
 
 char *strcat(char *destination, const char *source)
 {
 	/* TODO: Implement strcat(). */
+	strcpy(destination[strlen(source)], source);
 	return destination;
 }
 
 char *strncat(char *destination, const char *source, size_t len)
 {
 	/* TODO: Implement strncat(). */
+	size_t dest_len = strlen(destination);
+
+	for (size_t i = 0; i < dest_len; i++) {
+		destination[dest_len + i] = source[i];
+		if (source[i] == NULL)
+			return destination;
+	}
+	destination[dest_len + i + 1] = NULL;
 	return destination;
 }
 
 int strcmp(const char *str1, const char *str2)
 {
 	/* TODO: Implement strcmp(). */
-	return -1;
+	size_t len = strlen(str1);
+	for (size_t i = 0; i < len; i++) {
+		if (str1[i] < str2[i])
+			return -1;
+		if (str1[i] > str2[i])
+			return 1;
+	}
+	if (str2[i] != NULL)
+		return -1;
+	return 0;
 }
 
 int strncmp(const char *str1, const char *str2, size_t len)
 {
 	/* TODO: Implement strncmp(). */
-	return -1;
+	for (size_t i = 0; i < len; i++) {
+		if (str1[i] < str2[i])
+			return -1;
+		if (str1[i] > str2[i])
+			return 1;
+		if (str1[i] == NULL && str2[i] == NULL)
+			return 0;
+	}
+	return 0;
 }
 
 size_t strlen(const char *str)
@@ -51,12 +86,18 @@ size_t strlen(const char *str)
 char *strchr(const char *str, int c)
 {
 	/* TODO: Implement strchr(). */
+	size_t len = strlen(str1);
+	for (size_t i = 0; i < len; i++) {
+		if (str[i] == c)
+			return str + i;
+	}
 	return NULL;
 }
 
 char *strrchr(const char *str, int c)
 {
 	/* TODO: Implement strrchr(). */
+	
 	return NULL;
 }
 
